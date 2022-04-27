69b7f0f4c313d7f6b542b63c9f50461c2b9f381c
ralmeidati <ralmeida_ti@yahoo.com.br>
2022-04-27 11:46:41 -0300

    Mudança do servidor de BD

--- main.py
+++ main.py
@@ -10,46 +10,49 @@ from sqlalchemy import create_engine
 import pymysql
 
 def _carga():
-    con = mysql.connector.connect(host='academiadb1.mysql.database.azure.com',
-                              database='academia',
+    con = mysql.connector.connect(host='remotemysql.com',
+                              database='7Ap6ze1RVO',
                               user=st.secrets["db_username"],
                               password=st.secrets["db_password"])
     cursor = con.cursor()
-    df = pd.read_sql("SELECT * FROM DW_1\
-                WHERE NOT EXISTS(SELECT * FROM DW_2 WHERE DW_1.`Código de cliente` = DW_2.`Código de cliente`)\
+    df = pd.read_sql("SELECT * FROM dw1\
+                WHERE NOT EXISTS(SELECT * FROM dw2 WHERE dw1.`Código de cliente` = dw2.`Código de cliente`)\
+                ORDER BY `data última presença`\
                 limit 100", con);
     cursor.close()
     con.close()
     df.drop('index', axis=1, inplace = True)
     
-    sqlEngine = create_engine(st.secrets["azure_auth"], pool_recycle=3600)
+    sqlEngine = create_engine(st.secrets["remotemysql_auth"], pool_recycle=3600)
     dbConnection    = sqlEngine.connect()
-    df.to_sql('dw_2', dbConnection, index=False,if_exists='append');
+    df.to_sql('dw2', dbConnection, index=False,if_exists='append');
     dbConnection.close()
 
 def _reset():
-    con = mysql.connector.connect(host='academiadb1.mysql.database.azure.com',
-                              database='academia',
+    con = mysql.connector.connect(host='remotemysql.com',
+                              database='7Ap6ze1RVO',
                               user=st.secrets["db_username"],
-                              password=st.secrets["db_password"],)
+                              password=st.secrets["db_password"])
     cursor = con.cursor()
-    df = pd.read_sql("SELECT * FROM DW_1 limit 100", con);
+    df = pd.read_sql("SELECT * FROM dw1 limit 100", con);
     cursor.close()
     con.close()
     df.drop('index', axis=1, inplace = True)
-    sqlEngine = create_engine(st.secrets["azure_auth"], pool_recycle=3600)
+    sqlEngine = create_engine(st.secrets["remotemysql_auth"], pool_recycle=3600)
     dbConnection = sqlEngine.connect()
-    df.to_sql('dw_2', dbConnection, index=False, if_exists='replace');
+    df.to_sql('dw2', dbConnection, index=False, if_exists='replace');
     dbConnection.close()
 
 
-con = mysql.connector.connect(host='academiadb1.mysql.database.azure.com',
-                              database='academia',
+# Consulta inicial ao DW2 para carregar o programa:
+
+con = mysql.connector.connect(host='remotemysql.com',
+                              database='7Ap6ze1RVO',
                               user=st.secrets["db_username"],
                               password=st.secrets["db_password"])
 cursor = con.cursor()
 
-df = pd.read_sql("SELECT * FROM dw_2", con);
+df = pd.read_sql("SELECT * FROM dw2", con);
 
 cursor.close()
 con.close()
@@ -63,22 +66,14 @@ for d in datas:
 datas = pd.Series(periodo)
 periodo = datas.unique()
 
+hoje = datetime.now()
+Calc_idades = df[['Nascimento']]
+#Acrescento uma coluna no Calc_idades preenchendo com a data de hoje
+for idade in Calc_idades:    
+    Calc_idades['data_atual'] = hoje
+df['idade'] = (Calc_idades.data_atual[0:len(Calc_idades)] - Calc_idades.Nascimento[0:len(Calc_idades)]) // timedelta(days=365.2425)
 
-consultores = df[['Consultor']].groupby('Consultor').agg(Total=('Consultor','count')).sort_values(by='Total', ascending=False)
-consultor = consultores.head(1)
-
-professores = df[['Professor']].groupby('Professor').agg(Total=('Professor','count')).sort_values(by='Total', ascending=False)
-professores.head(8)
-professor = professores[1:2]
-
-temp = pd.DataFrame()
-temp[['modalidade1','modalidade2','modalidade3']] = df['Modalidade'].str.split('+', expand = True)
-correcao = {'MUSCULAÇÃO ': 'MUSCULAÇÃO',' MUSCULAÇÃO':'MUSCULAÇÃO',' MUSCULAÇÃO ':'MUSCULAÇÃO','FUNCIONAL ':'FUNCIONAL',' FUNCIONAL':'FUNCIONAL','NATAÇÃO ':'NATAÇÃO',' NATAÇÃO':'NATAÇÃO','-':'PLANO COMPLETO'}
-temp.replace(correcao, inplace= True)
-
-modalidades = pd.DataFrame()
-modalidades['Total'] = pd.concat([temp['modalidade1'],temp['modalidade2'],temp['modalidade3']], ignore_index=True)
-modalidades = pd.DataFrame(modalidades['Total'].value_counts())
+# O que é exibido no navegador começa aqui:
 
 st.title("Academia Pompeia")
 
@@ -86,7 +81,12 @@ st.sidebar.button('Carregar mais dados', help='Clique para realizar a simulaçã
 
 sidebar_selection = st.sidebar.radio(
     "Selecione uma opção:",
-    ['Qual é o cliente que não vai a mais tempo?','O consultor que tem mais clientes?','Professor que tem mais alunos?','Qual é a modalidade, mais consumida?','Encontrar inconsistencias no cadastro das modalidades?'],)
+    ['Qual é o cliente que não vai a mais tempo?',
+     'O consultor que tem mais clientes?',
+     'Professor que tem mais alunos?',
+     'Qual é a modalidade, mais consumida?',
+     'Como conheceu a academia',
+     'Encontrar inconsistencias no cadastro das modalidades?'])
 
 if sidebar_selection == 'Qual é o cliente que não vai a mais tempo?':
   st.markdown('## Qual é o cliente que não vai a mais tempo?')
@@ -111,6 +111,9 @@ if sidebar_selection == 'O consultor que tem mais clientes?':
   
 if sidebar_selection == 'Professor que tem mais alunos?':
   st.markdown('## Professor que tem mais alunos?')
+  professores = df[['Professor']].groupby('Professor').agg(Total=('Professor','count')).sort_values(by='Total', ascending=False)
+  professores.head(8)
+  professor = professores[1:2]
   professor
   st.bar_chart(professores,width=0, height=400)
   
@@ -118,11 +121,42 @@ if sidebar_selection == 'Qual é a modalidade, mais consumida?':
   st.markdown('## Qual é a modalidade, mais consumida?')
   st.write('Modalidades dentro do período')  
   data_inicial, data_final = st.select_slider('Selecione o período:', options=periodo, value=(periodo.min(), periodo.max()))
+  temp = pd.DataFrame({'datas':datas})
+  temp[['modalidade1','modalidade2','modalidade3']] = df['Modalidade'].str.split('+', expand = True)
+  correcao = {'MUSCULAÇÃO ': 'MUSCULAÇÃO',' MUSCULAÇÃO':'MUSCULAÇÃO',' MUSCULAÇÃO ':'MUSCULAÇÃO','FUNCIONAL ':'FUNCIONAL',' FUNCIONAL':'FUNCIONAL','NATAÇÃO ':'NATAÇÃO',' NATAÇÃO':'NATAÇÃO','-':'PLANO COMPLETO'}
+  temp.replace(correcao, inplace= True)
+  temp = temp[(temp['datas'] >= data_inicial) & (temp['datas'] <= data_final)]
+  modalidades = pd.DataFrame()
+  modalidades['Total'] = pd.concat([temp['modalidade1'],temp['modalidade2'],temp['modalidade3']], ignore_index=True)
+  modalidades = pd.DataFrame(modalidades['Total'].value_counts())
   modalidades[0:2]
   st.bar_chart(modalidades,width=0, height=400)
+
+if sidebar_selection == 'Como conheceu a academia':
+  df['Como Conheceu'].replace({'-':'NÃO INFORMADO'}, inplace = True)
+  como_conheceu = df['Como Conheceu'].value_counts()
+  st.bar_chart(como_conheceu,width=0, height=400)
+
+if sidebar_selection == 'Encontrar inconsistencias no cadastro das modalidades?':    
+  st.write("Clientes cadastrados como Natação Infantil que estão acima da idade")
+  select_1 = df[df['Modalidade'].str.contains('NATAÇÃO INFANTIL', na=False)]
+  select_1 = select_1[['Código de cliente','idade','Modalidade']]
+  select_1 = select_1.query('idade > 13').sort_values(by=['idade'])
+  select_1
   
-if sidebar_selection == 'Encontrar inconsistencias no cadastro das modalidades?':
-  st.write("Em análise...")
+  st.write("Clientes cadastrados como Natação que estão abaixo da idade")
+  select_2 = df[df['Modalidade'].str.contains('NATAÇÃO', na=False)]
+  select_2 = select_2[~select_2['Modalidade'].str.contains('INFANTIL', na=False)]
+  select_2 = select_2[['Código de cliente','idade','Modalidade']]
+  select_2 = select_2.query('idade < 13').sort_values(by=['idade'])
+  select_2
 
+  st.write("Clientes cadastrados como Musculação que estão abaixo da idade")
+  select_3 = df[df['Modalidade'].str.contains('MUSCULAÇÃO', na=False)]
+  select_3 = select_3[['Código de cliente','idade','Modalidade']]
+  select_3 = select_3.query('idade < 16').sort_values(by=['idade'])
+  select_3
+
+
+    
 st.sidebar.button('RESET', help='Clique para voltar ao estado inicial', on_click=_reset)
-
